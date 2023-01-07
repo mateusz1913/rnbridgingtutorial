177ff93c690e315afacfbb153811e81b1a0db546
Mateusz Mędrek <mateusz.medrek@netguru.com>
2023-01-08 00:07:10 +0100

    chore: upgrade to RN 0.71.0-rc.5, switch to new arch

--- android/app/src/main/java/com/rnbridgingtutorial/MainApplication.kt
+++ android/app/src/main/java/com/rnbridgingtutorial/MainApplication.kt
@@ -1,19 +1,16 @@
 package com.rnbridgingtutorial
 
 import android.app.Application
-import android.content.Context
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.config.ReactFeatureFlags
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-import com.rnbridgingtutorial.newarchitecture.MainApplicationReactNativeHost
-import java.lang.reflect.InvocationTargetException
 
 class MainApplication : Application(), ReactApplication {
-  private val mReactNativeHost = object : ReactNativeHost(this) {
+  private val mReactNativeHost = object : DefaultReactNativeHost(this) {
     override fun getUseDeveloperSupport(): Boolean {
       return BuildConfig.DEBUG
     }
@@ -28,58 +25,25 @@ class MainApplication : Application(), ReactApplication {
     override fun getJSMainModuleName(): String {
       return "index"
     }
+
+    override val isNewArchEnabled: Boolean
+      get() = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+
+    override val isHermesEnabled: Boolean
+      get() = BuildConfig.IS_HERMES_ENABLED
   }
 
-  private val mNewArchitectureNativeHost: ReactNativeHost = MainApplicationReactNativeHost(this)
-
   override fun getReactNativeHost(): ReactNativeHost {
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      return mNewArchitectureNativeHost
-    } else {
-      return mReactNativeHost
-    }
+    return mReactNativeHost
   }
 
   override fun onCreate() {
     super.onCreate()
-    // If you opted-in for the New Architecture, we enable the TurboModule system
-    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
     SoLoader.init(this, /* native exopackage */ false)
-    initializeFlipper(this, reactNativeHost.reactInstanceManager)
-  }
-
-  companion object {
-    /**
-     * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-     * initializeFlipper(this, reactNativeHost.reactInstanceManager)
-     *
-     * @param context
-     * @param reactInstanceManager
-     */
-    private fun initializeFlipper(
-      context: Context,
-      reactInstanceManager: ReactInstanceManager
-    ) {
-      if (BuildConfig.DEBUG) {
-        try {
-          val aClass = Class.forName("com.rnbridgingtutorial.ReactNativeFlipper")
-          aClass
-            .getMethod(
-              "initializeFlipper",
-              Context::class.java,
-              ReactInstanceManager::class.java
-            )
-            .invoke(null, context, reactInstanceManager)
-        } catch (e: ClassNotFoundException) {
-          e.printStackTrace()
-        } catch (e: NoSuchMethodException) {
-          e.printStackTrace()
-        } catch (e: IllegalAccessException) {
-          e.printStackTrace()
-        } catch (e: InvocationTargetException) {
-          e.printStackTrace()
-        }
-      }
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      DefaultNewArchitectureEntryPoint.load()
     }
+    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
   }
 }
